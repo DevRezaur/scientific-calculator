684d9bb704a88128856908b2689393babed2b7f1
Rezaur Rahman <rezaur.official@gmail.com>
2020-10-30 20:34:34 +0600

    Added: Scientific functionality

--- app/src/main/java/dev/rezaur/mycalculator/MainActivity.java
+++ app/src/main/java/dev/rezaur/mycalculator/MainActivity.java
@@ -7,17 +7,29 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView subPanel;
     private TextView mainPanel;
     private Double firstNumber;
     private Double secondNumber;
+    private SwitchMaterial degreeSwitch;
     private boolean plusFlag = false;
     private boolean minusFlag = false;
     private boolean multiplyFlag = false;
     private boolean divideFlag = false;
+    private boolean rootFlag = false;
+    private boolean modFlag = false;
+    private boolean aToBFlag = false;
+    private boolean tenToXFlag = false;
+    private boolean sinFlag = false;
+    private boolean cosFlag = false;
+    private boolean tanFlag = false;
+    private boolean logFlag = false;
     private boolean ansFlag = false;
+    private boolean degreeFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         subPanel = findViewById(R.id.subPanel);
         mainPanel = findViewById(R.id.mainPanel);
+        degreeSwitch = findViewById(R.id.degreeSwitch);
     }
 
     public void falseAllFlag() {
@@ -48,10 +61,10 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        if(!mainPanel.getText().toString().trim().equals("")) {
+        if(!mainPanel.getText().toString().equals("")) {
             plusFlag = true;
-            firstNumber = new Double(mainPanel.getText().toString().trim());
-            subPanel.setText(subPanel.getText().toString().trim() + mainPanel.getText().toString().trim() + "+");
+            firstNumber = new Double(mainPanel.getText().toString());
+            subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString() + "+");
             mainPanel.setText("");
         }
     }
@@ -62,13 +75,13 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        if(!mainPanel.getText().toString().trim().equals("") && !mainPanel.getText().toString().trim().equals("-")) {
+        if(!mainPanel.getText().toString().equals("") && !mainPanel.getText().toString().equals("-")) {
             minusFlag = true;
-            firstNumber = new Double(mainPanel.getText().toString().trim());
-            subPanel.setText(subPanel.getText().toString().trim() + mainPanel.getText().toString().trim() + "-");
+            firstNumber = new Double(mainPanel.getText().toString());
+            subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString() + "-");
             mainPanel.setText("");
         } else {
-            mainPanel.setText(mainPanel.getText().toString().trim() + "-");
+            mainPanel.setText(mainPanel.getText().toString() + "-");
         }
     }
 
@@ -78,10 +91,10 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        if(!mainPanel.getText().toString().trim().equals("")) {
+        if(!mainPanel.getText().toString().equals("")) {
             multiplyFlag = true;
-            firstNumber = new Double(mainPanel.getText().toString().trim());
-            subPanel.setText(subPanel.getText().toString().trim() + mainPanel.getText().toString().trim() + "*");
+            firstNumber = new Double(mainPanel.getText().toString());
+            subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString() + "*");
             mainPanel.setText("");
         }
     }
@@ -92,43 +105,101 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        if(!mainPanel.getText().toString().trim().equals("")) {
+        if(!mainPanel.getText().toString().equals("")) {
             divideFlag = true;
-            firstNumber = new Double(mainPanel.getText().toString().trim());
-            subPanel.setText(subPanel.getText().toString().trim() + mainPanel.getText().toString().trim() + "/");
+            firstNumber = new Double(mainPanel.getText().toString());
+            subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString() + "/");
             mainPanel.setText("");
         }
     }
 
     public void equalMethod(View view) {
-        if(!mainPanel.getText().toString().trim().equals("")) {
+        if(!mainPanel.getText().toString().equals("")) {
             if (plusFlag) {
                 ansFlag = true;
-                secondNumber = new Double(mainPanel.getText().toString().trim());
-                subPanel.setText(subPanel.getText().toString().trim() + mainPanel.getText().toString().trim());
+                secondNumber = new Double(mainPanel.getText().toString());
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
                 mainPanel.setText((firstNumber + secondNumber) + "");
                 plusFlag = false;
             } else if (minusFlag) {
                 ansFlag = true;
-                secondNumber = new Double(mainPanel.getText().toString().trim());
-                subPanel.setText(subPanel.getText().toString().trim() + mainPanel.getText().toString().trim());
+                secondNumber = new Double(mainPanel.getText().toString());
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
                 mainPanel.setText((firstNumber - secondNumber) + "");
                 minusFlag = false;
             } else if (multiplyFlag) {
                 ansFlag = true;
-                secondNumber = new Double(mainPanel.getText().toString().trim());
-                subPanel.setText(subPanel.getText().toString().trim() + mainPanel.getText().toString().trim());
+                secondNumber = new Double(mainPanel.getText().toString());
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
                 mainPanel.setText((firstNumber * secondNumber) + "");
                 multiplyFlag = false;
             } else if (divideFlag) {
                 ansFlag = true;
-                secondNumber = new Double(mainPanel.getText().toString().trim());
-                subPanel.setText(subPanel.getText().toString().trim() + mainPanel.getText().toString().trim());
+                secondNumber = new Double(mainPanel.getText().toString());
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
                 mainPanel.setText((firstNumber / secondNumber) + "");
                 divideFlag = false;
-            } else {
+            } else if (rootFlag) {
                 ansFlag = true;
-                subPanel.setText(mainPanel.getText().toString().trim());
+                firstNumber = new Double(mainPanel.getText().toString().substring(1));
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
+                mainPanel.setText((Math.sqrt(firstNumber)) + "");
+                rootFlag = false;
+            } else if (modFlag) {
+                ansFlag = true;
+                secondNumber = new Double(mainPanel.getText().toString());
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
+                mainPanel.setText((firstNumber % secondNumber) + "");
+                modFlag = false;
+            } else if (aToBFlag) {
+                ansFlag = true;
+                secondNumber = new Double(mainPanel.getText().toString());
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
+                mainPanel.setText((Math.pow(firstNumber, secondNumber)) + "");
+                aToBFlag = false;
+            } else if (tenToXFlag) {
+                ansFlag = true;
+                firstNumber = new Double(mainPanel.getText().toString().substring(3));
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
+                mainPanel.setText((Math.pow(10, firstNumber)) + "");
+                tenToXFlag = false;
+            } else if (sinFlag) {
+                ansFlag = true;
+                firstNumber = new Double(mainPanel.getText().toString().substring(4));
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
+                if(degreeSwitch.isChecked())
+                    mainPanel.setText((Math.sin(firstNumber * Math.PI/180)) + "");
+                else
+                    mainPanel.setText((Math.sin(firstNumber)) + "");
+                sinFlag = false;
+            } else if (cosFlag) {
+                ansFlag = true;
+                firstNumber = new Double(mainPanel.getText().toString().substring(4));
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
+                if(degreeSwitch.isChecked())
+                    mainPanel.setText((Math.cos(firstNumber * Math.PI/180)) + "");
+                else
+                    mainPanel.setText((Math.cos(firstNumber)) + "");
+                cosFlag = false;
+            } else if (tanFlag) {
+                ansFlag = true;
+                firstNumber = new Double(mainPanel.getText().toString().substring(4));
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
+                if(degreeSwitch.isChecked())
+                    mainPanel.setText((Math.tan(firstNumber * Math.PI/180)) + "");
+                else
+                    mainPanel.setText((Math.tan(firstNumber)) + "");
+                tanFlag = false;
+            } else if (logFlag) {
+                ansFlag = true;
+                firstNumber = new Double(mainPanel.getText().toString().substring(4));
+                subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString());
+                mainPanel.setText((Math.log10(firstNumber)) + "");
+                logFlag = false;
+            }
+            else {
+                ansFlag = true;
+                subPanel.setText(mainPanel.getText().toString());
             }
         } else {
             Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
@@ -141,10 +212,10 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        if(mainPanel.getText().toString().trim().equals(""))
+        if(mainPanel.getText().toString().equals(""))
             mainPanel.setText("0.");
         else
-            mainPanel.setText(mainPanel.getText().toString().trim() + ".");
+            mainPanel.setText(mainPanel.getText().toString() + ".");
     }
 
     public void zeroPressed(View view) {
@@ -153,7 +224,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "0");
+        mainPanel.setText(mainPanel.getText().toString() + "0");
     }
 
     public void onePressed(View view) {
@@ -162,7 +233,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "1");
+        mainPanel.setText(mainPanel.getText().toString() + "1");
     }
 
     public void twoPressed(View view) {
@@ -171,7 +242,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "2");
+        mainPanel.setText(mainPanel.getText().toString() + "2");
     }
 
     public void threePressed(View view) {
@@ -180,7 +251,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "3");
+        mainPanel.setText(mainPanel.getText().toString() + "3");
     }
 
     public void fourPressed(View view) {
@@ -189,7 +260,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "4");
+        mainPanel.setText(mainPanel.getText().toString() + "4");
     }
 
     public void fivePressed(View view) {
@@ -198,7 +269,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "5");
+        mainPanel.setText(mainPanel.getText().toString() + "5");
     }
 
     public void sixPressed(View view) {
@@ -207,7 +278,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "6");
+        mainPanel.setText(mainPanel.getText().toString() + "6");
     }
 
     public void sevenPressed(View view) {
@@ -216,7 +287,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "7");
+        mainPanel.setText(mainPanel.getText().toString() + "7");
     }
 
     public void eightPressed(View view) {
@@ -225,7 +296,7 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "8");
+        mainPanel.setText(mainPanel.getText().toString() + "8");
     }
 
     public void ninePressed(View view) {
@@ -234,6 +305,118 @@ public class MainActivity extends AppCompatActivity {
             subPanel.setText("");
             falseAllFlag();
         }
-        mainPanel.setText(mainPanel.getText().toString().trim() + "9");
+        mainPanel.setText(mainPanel.getText().toString() + "9");
+    }
+
+    public void sinPressed(View view) {
+        if(ansFlag) {
+            mainPanel.setText("");
+            subPanel.setText("");
+            falseAllFlag();
+        }
+        if(!mainPanel.getText().toString().equals("sin ")) {
+            sinFlag = true;
+            mainPanel.setText("sin ");
+        }
+    }
+
+    public void cosPressed(View view) {
+        if(ansFlag) {
+            mainPanel.setText("");
+            subPanel.setText("");
+            falseAllFlag();
+        }
+        if(!mainPanel.getText().toString().equals("cos ")) {
+            cosFlag = true;
+            mainPanel.setText("cos ");
+        }
+    }
+
+    public void tanPressed(View view) {
+        if(ansFlag) {
+            mainPanel.setText("");
+            subPanel.setText("");
+            falseAllFlag();
+        }
+        if(!mainPanel.getText().toString().equals("tan ")) {
+            tanFlag = true;
+            mainPanel.setText("tan ");
+        }
+    }
+
+    public void logPressed(View view) {
+        if(ansFlag) {
+            mainPanel.setText("");
+            subPanel.setText("");
+            falseAllFlag();
+        }
+        if(!mainPanel.getText().toString().equals("log ")) {
+            logFlag = true;
+            mainPanel.setText("log ");
+        }
+    }
+
+    public void rootPressed(View view) {
+        if(ansFlag) {
+            mainPanel.setText("");
+            subPanel.setText("");
+            falseAllFlag();
+        }
+        if(!mainPanel.getText().toString().equals("√")) {
+            rootFlag = true;
+            mainPanel.setText("√");
+        }
+    }
+
+    public void modPressed(View view) {
+        if(ansFlag) {
+            mainPanel.setText("");
+            subPanel.setText("");
+            falseAllFlag();
+        }
+        if(!mainPanel.getText().toString().equals("")) {
+            modFlag = true;
+            firstNumber = new Double(mainPanel.getText().toString());
+            subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString() + "%");
+            mainPanel.setText("");
+        }
+    }
+
+    public void aToBPressed(View view) {
+        if(ansFlag) {
+            mainPanel.setText("");
+            subPanel.setText("");
+            falseAllFlag();
+        }
+        if(!mainPanel.getText().toString().equals("")) {
+            aToBFlag = true;
+            firstNumber = new Double(mainPanel.getText().toString());
+            subPanel.setText(subPanel.getText().toString() + mainPanel.getText().toString() + "^");
+            mainPanel.setText("");
+        }
+    }
+
+    public void tenToXPressed(View view) {
+        if(ansFlag) {
+            mainPanel.setText("");
+            subPanel.setText("");
+            falseAllFlag();
+        }
+        if(!mainPanel.getText().toString().equals("10^55")) {
+            tenToXFlag = true;
+            mainPanel.setText("10^");
+        }
+    }
+
+    public void radianToDegree(View view) {
+        if(ansFlag && !degreeFlag) {
+            degreeFlag = true;
+            Double temp = new Double(mainPanel.getText().toString());
+            mainPanel.setText((temp * (Math.PI/180)) + "");
+        } else if(ansFlag && degreeFlag) {
+            degreeFlag = false;
+            Double temp = new Double(mainPanel.getText().toString());
+            mainPanel.setText((temp / (Math.PI/180)) + "");
+        }
     }
 }
